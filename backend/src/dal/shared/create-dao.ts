f19217b25af0b8b9dae6456a394c89f78af5382e
artem <r.tem@tuta.io>
2022-06-14 13:56:28 +0300

    temporary restored non-refactored daos

--- backend/src/dal/shared/create-dao.ts
+++ backend/src/dal/shared/create-dao.ts
@@ -1,31 +1,31 @@
 import { ConnectionTypeEnum } from '../../enums';
 import { DaoAgent } from '../dao-agent/dao-agent';
+import { DaoMssql } from '../dao/dao-mssql';
+import { DaoMysql } from '../dao/dao-mysql';
+import { DaoOracledb } from '../dao/dao-oracledb';
+import { DaoPostgres } from '../dao/dao-postgres';
+import { DaoSshMysql } from '../dao-ssh/dao-ssh-mysql';
 import { HttpException } from '@nestjs/common/exceptions/http.exception';
 import { HttpStatus } from '@nestjs/common';
 import { IDaoInterface } from './dao-interface';
-import { DataAccessObjectPostgres } from '../../data-access-layer/data-access-objects/data-access-object-postgres';
-import { DataAccessObjectMysql } from '../../data-access-layer/data-access-objects/data-access-object-mysql';
-import { DataAccessObjectMysqlSsh } from '../../data-access-layer/data-access-objects/data-access-object-mysql-ssh';
-import { DataAccessObjectOracle } from '../../data-access-layer/data-access-objects/data-access-object-oracle';
-import { DataAccessObjectMssql } from '../../data-access-layer/data-access-objects/data-access-object-mssql';
 
 export function createDao(connection, cognitoUserName: string): IDaoInterface {
   switch (connection.type) {
     case ConnectionTypeEnum.postgres:
-      return new DataAccessObjectPostgres(connection) as any;
+      return new DaoPostgres(connection);
 
     case ConnectionTypeEnum.mysql:
       if (connection.ssh) {
-        return new DataAccessObjectMysqlSsh(connection) as any;
+        return new DaoSshMysql(connection);
       } else {
-        return new DataAccessObjectMysql(connection) as any;
+        return new DaoMysql(connection);
       }
 
     case ConnectionTypeEnum.oracledb:
-      return new DataAccessObjectOracle(connection) as any;
+      return new DaoOracledb(connection);
 
     case ConnectionTypeEnum.mssql:
-      return new DataAccessObjectMssql(connection) as any;
+      return new DaoMssql(connection);
 
     case ConnectionTypeEnum.agent_postgres:
     case ConnectionTypeEnum.agent_mssql:
