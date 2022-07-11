acd8e893e83128230d21bbe70493ccb793ab8daf
artem <r.tem@tuta.io>
2022-07-11 16:06:33 +0300

    removed user access service from guards

--- backend/src/guards/connection-edit.guard.ts
+++ backend/src/guards/connection-edit.guard.ts
@@ -1,16 +1,17 @@
-import { CanActivate, ExecutionContext, forwardRef, Inject, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { IRequestWithCognitoInfo } from '../authorization';
-import { UserAccessService } from '../entities/user-access/user-access.service';
 import { Messages } from '../exceptions/text/messages';
 import { buildBadRequestException, buildForbiddenException } from './utils';
 import { validateUuidByRegex } from './utils/validate-uuid-by-regex';
+import { BaseType } from '../common/data-injection.tokens';
+import { IGlobalDatabaseContext } from '../common/application/global-database-context.intarface';
 
 @Injectable()
 export class ConnectionEditGuard implements CanActivate {
   constructor(
-    @Inject(forwardRef(() => UserAccessService))
-    private readonly userAccessService: UserAccessService,
+    @Inject(BaseType.GLOBAL_DB_CONTEXT)
+    protected _dbContext: IGlobalDatabaseContext,
   ) {}
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
@@ -27,7 +28,10 @@ export class ConnectionEditGuard implements CanActivate {
       }
       let userConnectionEdit = false;
       try {
-        userConnectionEdit = await this.userAccessService.checkUserConnectionEdit(cognitoUserName, connectionId);
+        userConnectionEdit = await this._dbContext.userAccessRepository.checkUserConnectionEdit(
+          cognitoUserName,
+          connectionId,
+        );
       } catch (e) {
         reject(e);
         return;
