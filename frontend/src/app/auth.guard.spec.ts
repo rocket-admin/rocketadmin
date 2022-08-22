3e425756025b1532d3561c799ba78a18d36428c7
Lyubov Voloshko <lyubov@voloshko.com>
2022-08-22 11:33:59 +0300

    unit tests: fix auth guard.

--- frontend/src/app/auth.guard.spec.ts
+++ frontend/src/app/auth.guard.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthGuard]
+      providers: [AuthGuard],
+      imports: [RouterTestingModule]
     });
   });
 
