e636e5eaa0828b369fa58441a23caab1f9a9e8db
artem <r.tem@tuta.io>
2022-08-24 13:14:32 +0300

    validation => ValidationHelper

--- backend/src/decorators/slug-uuid.decorator.ts
+++ backend/src/decorators/slug-uuid.decorator.ts
@@ -1,13 +1,13 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { IRequestWithCognitoInfo } from '../authorization';
-import { validate as uuidValidate } from 'uuid';
 import { buildBadRequestException } from '../guards/utils';
 import { Messages } from '../exceptions/text/messages';
+import { ValidationHelper } from '../helpers/validators/ValidationHelper';
 
 export const SlugUuid = createParamDecorator((data: any, ctx: ExecutionContext): string => {
   const request: IRequestWithCognitoInfo = ctx.switchToHttp().getRequest();
   const uuId: string = request.params?.slug;
-  const validationResult = uuidValidate(uuId);
+  const validationResult = ValidationHelper.isValidUUID(uuId);
   if (validationResult) {
     return uuId;
   }
