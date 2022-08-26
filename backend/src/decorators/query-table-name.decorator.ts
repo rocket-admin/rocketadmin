29b7e0180e423f923622d4c4a48fea58b82b8eeb
artem <r.tem@tuta.io>
2022-08-26 13:59:23 +0300

    added table name length check in table name query decorator

--- backend/src/decorators/query-table-name.decorator.ts
+++ backend/src/decorators/query-table-name.decorator.ts
@@ -6,7 +6,7 @@ import { buildBadRequestException } from '../guards/utils';
 export const QueryTableName = createParamDecorator((data: unknown, ctx: ExecutionContext): string => {
   const request: IRequestWithCognitoInfo = ctx.switchToHttp().getRequest();
   const query = request.query;
-  if (query.hasOwnProperty('tableName')) {
+  if (query.hasOwnProperty('tableName') && query['tableName'].length > 0) {
     return query['tableName'];
   }
   throw buildBadRequestException(Messages.TABLE_NAME_MISSING);
