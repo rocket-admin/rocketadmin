dadee1e475d0c88bdd5a1bed961bc469ee617bf1
artem <r.tem@tuta.io>
2022-08-26 11:20:15 +0300

    import fixed

--- backend/src/decorators/query-table-name.decorator.ts
+++ backend/src/decorators/query-table-name.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { buildBadRequestException } from 'src/guards/utils';
 import { IRequestWithCognitoInfo } from '../authorization';
 import { Messages } from '../exceptions/text/messages';
+import { buildBadRequestException } from '../guards/utils';
 
 export const QueryTableName = createParamDecorator((data: unknown, ctx: ExecutionContext): string => {
   const request: IRequestWithCognitoInfo = ctx.switchToHttp().getRequest();
