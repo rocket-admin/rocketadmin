6310c8dd8de0f648f8b1bd164a14b84bdf8677ba
Lyubov Voloshko <lyubov@voloshko.com>
2022-10-17 17:13:20 +0300

    add connection: add message for google authorized networks.

--- frontend/src/app/components/connect-db/db-connection-ip-access-dialog/db-connection-ip-access-dialog.component.ts
+++ frontend/src/app/components/connect-db/db-connection-ip-access-dialog/db-connection-ip-access-dialog.component.ts
@@ -2,6 +2,9 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NotificationsService } from 'src/app/services/notifications.service';
 import { Connection } from 'src/app/models/connection';
+import googlIPsList from 'src/app/consts/google-IP-addresses';
+import * as ipaddr from 'ipaddr.js';
+import isIP from 'validator/lib/isIP';
 
 @Component({
   selector: 'app-db-connection-ip-access-dialog',
@@ -21,6 +24,15 @@ export class DbConnectionIpAccessDialogComponent implements OnInit {
   ngOnInit(): void {
     if (this.db.host.endsWith('.amazonaws.com')) this.provider = 'amazon';
     if (this.db.host.endsWith('.azure.com')) this.provider = 'azure';
+    if(isIP(this.db.host)) {
+      const hostIP = ipaddr.parse(this.db.host);
+      for (const addr of googlIPsList) {
+        if (hostIP.match(ipaddr.parseCIDR(addr))) {
+          this.provider = 'google';
+          return;
+        }
+      }
+    }
   }
 
   showCopyNotification(message: string) {
