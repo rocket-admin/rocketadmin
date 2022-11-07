9c536ce5901194d5be377bce42a7b7fe0081a88e
Artem <r.tem@tuta.io>
2022-11-07 14:18:44 +0000

    add name peroperty in user entity add migration what

--- backend/src/migrations/1666959132601-AddCanDeleteUpdateAddOptionsInTableSettingsEntity.ts
+++ backend/src/migrations/1666959132601-AddCanDeleteUpdateAddOptionsInTableSettingsEntity.ts
@@ -1,18 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddCanDeleteUpdateAddOptionsInTableSettingsEntity1666959132601 implements MigrationInterface {
-    name = 'AddCanDeleteUpdateAddOptionsInTableSettingsEntity1666959132601'
+  name = 'AddCanDeleteUpdateAddOptionsInTableSettingsEntity1666959132601';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "tableSettings" ADD "can_delete" boolean NOT NULL DEFAULT true`);
-        await queryRunner.query(`ALTER TABLE "tableSettings" ADD "can_update" boolean NOT NULL DEFAULT true`);
-        await queryRunner.query(`ALTER TABLE "tableSettings" ADD "can_add" boolean NOT NULL DEFAULT true`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "tableSettings" ADD "can_delete" boolean NOT NULL DEFAULT true`);
+    await queryRunner.query(`ALTER TABLE "tableSettings" ADD "can_update" boolean NOT NULL DEFAULT true`);
+    await queryRunner.query(`ALTER TABLE "tableSettings" ADD "can_add" boolean NOT NULL DEFAULT true`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "tableSettings" DROP COLUMN "can_add"`);
-        await queryRunner.query(`ALTER TABLE "tableSettings" DROP COLUMN "can_update"`);
-        await queryRunner.query(`ALTER TABLE "tableSettings" DROP COLUMN "can_delete"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "tableSettings" DROP COLUMN "can_add"`);
+    await queryRunner.query(`ALTER TABLE "tableSettings" DROP COLUMN "can_update"`);
+    await queryRunner.query(`ALTER TABLE "tableSettings" DROP COLUMN "can_delete"`);
+  }
 }
