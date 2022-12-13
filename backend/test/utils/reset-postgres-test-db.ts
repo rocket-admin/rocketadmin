da239dbed4c7895ea48b7694fdbd26aab90e7b72
Artem <r.tem@tuta.io>
2022-12-13 12:38:12 +0000

    fix table action (icons) added tests

---
+++ backend/test/utils/reset-postgres-test-db.ts
@@ -0,0 +1,39 @@
+export async function resetPostgresTestDB(newConnection) {
+  const { host, username, password, database, port, type, ssl, cert } = newConnection;
+  const Knex = knex({
+    client: type,
+    connection: {
+      host: host,
+      user: username,
+      password: password,
+      database: database,
+      port: port,
+    },
+  });
+  await Knex.schema.dropTableIfExists(testTableName);
+  await Knex.schema.createTableIfNotExists(testTableName, function (table) {
+    table.increments();
+    table.string(testTableColumnName);
+    table.string(testTAbleSecondColumnName);
+    table.timestamps();
+  });
+
+  for (let i = 0; i < testEntitiesSeedsCount; i++) {
+    if (i === 0 || i === testEntitiesSeedsCount - 21 || i === testEntitiesSeedsCount - 5) {
+      await Knex(testTableName).insert({
+        [testTableColumnName]: testSearchedUserName,
+        [testTAbleSecondColumnName]: faker.internet.email(),
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    } else {
+      await Knex(testTableName).insert({
+        [testTableColumnName]: faker.name.findName(),
+        [testTAbleSecondColumnName]: faker.internet.email(),
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    }
+  }
+  await Knex.destroy();
+}
