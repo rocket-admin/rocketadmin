e4d6bd3f0d09a9f4f110fe081c8433f03fd817ce
Artem Niehrieiev <r.tem@tuta.io>
2023-01-12 15:16:02 +0000

    add e2e ava tests

--- backend/test/utils/user-with-different-permissions-utils.ts
+++ backend/test/utils/user-with-different-permissions-utils.ts
@@ -370,6 +370,131 @@ export async function createConnectionsAndInviteNewUserInNewGroupWithOnlyTablePe
   };
 }
 
+export async function createConnectionsAndInviteNewUserInNewGroupWithTableDifferentConnectionGroupReadOnlyPermissions(
+  app: INestApplication,
+): Promise<IUserDifferentTableOnlyPermissionsFooData> {
+  const connectionsId = {
+    firstId: null,
+    secondId: null,
+    firstAdminGroupId: null,
+  };
+  const mockFactory = new MockFactory();
+  const connectionAdminUserInfo = await registerUserAndReturnUserInfo(app);
+  const simpleUserRegisterInfo = await registerUserAndReturnUserInfo(app);
+  const connectionAdminUserToken = connectionAdminUserInfo.token;
+  const simpleUserToken = simpleUserRegisterInfo.token;
+
+  const newConnection = mockFactory.generateConnectionToTestPostgresDBInDocker();
+  const newConnection2 = mockFactory.generateConnectionToTestMySQLDBInDocker();
+  const newGroup1 = mockFactory.generateCreateGroupDto1();
+  const firstTable = await createTestTable(newConnection);
+  const secondTable = await createTestTable(newConnection2);
+
+  const tablePermissions = {
+    visibility: true,
+    readonly: false,
+    add: true,
+    delete: true,
+    edit: false,
+  };
+
+  const createFirstConnectionResponse = await request(app.getHttpServer())
+    .post('/connection')
+    .set('Cookie', connectionAdminUserToken)
+    .send(newConnection)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json');
+  const createFirstConnectionRO = JSON.parse(createFirstConnectionResponse.text);
+  connectionsId.firstId = createFirstConnectionRO.id;
+
+  const getGroupsResponse = await request(app.getHttpServer())
+    .get(`/connection/groups/${connectionsId.firstId}`)
+    .set('Cookie', connectionAdminUserToken)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json');
+  const getGroupsRO = JSON.parse(getGroupsResponse.text);
+
+  const firstAdminGroupId = getGroupsRO[0].group.id;
+
+  const createSecondConnectionResponse = await request(app.getHttpServer())
+    .post('/connection')
+    .set('Cookie', connectionAdminUserToken)
+    .send(newConnection2)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json');
+  const createSecondConnectionRO = JSON.parse(createSecondConnectionResponse.text);
+  connectionsId.secondId = createSecondConnectionRO.id;
+  const email = simpleUserRegisterInfo.email;
+
+  const createGroupResponse = await request(app.getHttpServer())
+    .post(`/connection/group/${connectionsId.firstId}`)
+    .set('Cookie', connectionAdminUserToken)
+    .send(newGroup1)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json');
+  const groupId = JSON.parse(createGroupResponse.text).id;
+
+  const permissions = {
+    connection: {
+      connectionId: connectionsId.firstId,
+      accessLevel: AccessLevelEnum.readonly,
+    },
+    group: {
+      groupId: groupId,
+      accessLevel: AccessLevelEnum.readonly,
+    },
+    tables: [
+      {
+        tableName: firstTable.testTableName,
+        accessLevel: tablePermissions,
+      },
+    ],
+  };
+
+  const createOrUpdatePermissionResponse = await request(app.getHttpServer())
+    .put(`/permissions/${groupId}`)
+    .send({ permissions })
+    .set('Cookie', connectionAdminUserToken)
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json');
+
+  await request(app.getHttpServer())
+    .put('/group/user')
+    .set('Cookie', connectionAdminUserToken)
+    .send({ groupId, email })
+    .set('Content-Type', 'application/json')
+    .set('Accept', 'application/json');
+
+  return {
+    firstTableInfo: firstTable,
+    secondTableInfo: secondTable,
+    permissions: {
+      table: {
+        visibility: true,
+        readonly: false,
+        add: true,
+        delete: true,
+        edit: false,
+      },
+    },
+    connections: {
+      firstId: connectionsId.firstId,
+      secondId: connectionsId.secondId,
+    },
+    groups: {
+      firstAdminGroupId: firstAdminGroupId,
+      secondAdminGroupId: null,
+      createdGroupId: groupId,
+    },
+    users: {
+      adminUserToken: connectionAdminUserToken,
+      simpleUserToken: simpleUserToken,
+      simpleUserEmail: simpleUserRegisterInfo.email,
+      adminUserEmail: connectionAdminUserInfo.email,
+    },
+  };
+}
+
 export async function createConnectionsAndInviteNewUserInAdminGroupOfFirstConnection(
   app: INestApplication,
 ): Promise<IUserDifferentTableOnlyPermissionsFooData> {
