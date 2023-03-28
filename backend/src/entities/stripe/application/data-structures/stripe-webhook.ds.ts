fd19610e5aabe5d841f7d3a2ef77ab9f3d5fb55b
Artem Niehrieiev <r.tem@tuta.io>
2023-03-28 10:58:11 +0300

    added stripe private module and stripe private service

--- backend/src/entities/stripe/application/data-structures/stripe-webhook.ds.ts
+++ backend/src/entities/stripe/application/data-structures/stripe-webhook.ds.ts
@@ -1,4 +1,3 @@
-
 export class StripeWebhookDS {
   request: any;
   stripeSigranture: string;
--- backend/src/entities/stripe/stripe.module.ts
+++ backend/src/entities/stripe/stripe.module.ts
@@ -1,6 +1,7 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { GlobalDatabaseContext } from '../../common/application/global-database-context.js';
 import { BaseType, UseCaseType } from '../../common/data-injection.tokens.js';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
+import { GlobalDatabaseContext } from '../../common/application/global-database-context.js';
 import { StripeWebhookController } from './stripe.controller.js';
 import { StripeWebhookUseCase } from './use-cases/stripe-webhook.use.case.js';
 
