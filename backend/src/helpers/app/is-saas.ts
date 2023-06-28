138722430363d944fc388925453cf662ac9e8ba5
Artem Niehrieiev <r.tem@tuta.io>
2023-06-28 14:13:33 +0200

    isSAAS Fix

--- backend/src/helpers/app/is-saas.ts
+++ backend/src/helpers/app/is-saas.ts
@@ -2,14 +2,5 @@ import { getProcessVariable } from '../get-process-variable.js';
 
 export function isSaaS(): boolean {
   const isSaaS: unknown = getProcessVariable('IS_SAAS');
-  if (typeof isSaaS === 'string') {
-    return isSaaS.toLowerCase() === 'true';
-  }
-  if (typeof isSaaS === 'boolean') {
-    return isSaaS;
-  }
-  if (typeof isSaaS === 'number') {
-    return isSaaS === 1;
-  }
-  return false;
+  return !!isSaaS;
 }
