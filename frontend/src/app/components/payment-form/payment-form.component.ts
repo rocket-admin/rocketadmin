963b9aaaf46af18a9454f446417c7f4ef46e94ac
Lyubov Voloshko <lyubov@voloshko.com>
2023-07-18 15:07:17 +0300

    payment form and data checking

--- frontend/src/app/components/payment-form/payment-form.component.ts
+++ frontend/src/app/components/payment-form/payment-form.component.ts
@@ -4,7 +4,8 @@ import { UserService } from 'src/app/services/user.service';
 import { StripeService, StripePaymentElementComponent } from 'ngx-stripe';
 import {
   StripeElementsOptions,
-  PaymentIntent
+  PaymentIntent,
+  StripeCardElementOptions
 } from '@stripe/stripe-js';
 import { HttpClient } from '@angular/common/http';
 import { FormBuilder, Validators } from '@angular/forms';
@@ -20,7 +21,22 @@ export class PaymentFormComponent implements OnInit {
   paymentElement: StripePaymentElementComponent;
 
   elementsOptions: StripeElementsOptions = {
-    locale: 'en'
+    locale: 'en',
+  };
+
+  cardOptions: StripeCardElementOptions = {
+    style: {
+      base: {
+        iconColor: '#666EE8',
+        color: '#31325F',
+        fontWeight: '300',
+        fontFamily: 'Poppins, sans-serif',
+        fontSize: '18px',
+        '::placeholder': {
+          color: '#CFD7E0',
+        },
+      },
+    },
   };
 
   // public paymentElementForm;
@@ -34,21 +50,61 @@ export class PaymentFormComponent implements OnInit {
     private stripeService: StripeService
   ) { }
 
-  public paymentElementForm = this.fb.group({
-    name: ['John doe', [Validators.required]],
-    email: ['support@ngx-stripe.dev', [Validators.required]],
-    address: [''],
-    zipcode: [''],
-    city: [''],
-    amount: [2500, [Validators.required, Validators.pattern(/d+/)]]
-  });
+  public paymentElementForm;
 
   ngOnInit(): void {
+    this.paymentElementForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      address: [''],
+      zipcode: [''],
+      city: ['']
+    });
+
     this._userService.cast.subscribe(user => {
       if (user) this._paymentService.intentToPay().subscribe(res => {
         console.log(res);
+        this.elementsOptions.clientSecret = res.stripeIntentSecret;
       });
     });
+  }
 
+  pay() {
+    if (this.paymentElementForm.valid) {
+      this.paying = true;
+      this.stripeService.confirmSetup({
+        elements: this.paymentElement.elements,
+        confirmParams: {
+          payment_method_data: {
+            billing_details: {
+              name: this.paymentElementForm.get('name').value,
+              email: this.paymentElementForm.get('email').value,
+              address: {
+                line1: this.paymentElementForm.get('address').value || '',
+                postal_code: this.paymentElementForm.get('zipcode').value || '',
+                city: this.paymentElementForm.get('city').value || '',
+              }
+            }
+          }
+        },
+        redirect: 'if_required'
+      }).subscribe(result => {
+        this.paying = false;
+        console.log('Result', result);
+        if (result.error) {
+          // Show error to your customer (e.g., insufficient funds)
+          console.log({ success: false, error: result.error.message });
+        } else {
+          // The payment has been processed!
+          if (result.setupIntent.status === 'succeeded') {
+            // Show a success message to your customer
+            console.log({ success: true });
+            console.log(result.setupIntent.client_secret);
+          }
+        }
+      });
+    } else {
+      console.log(this.paymentElementForm);
+    }
   }
 }
