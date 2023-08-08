0fcb10123ba845558c21b0b2144c7091f66aa3e6
Artem Niehrieiev <r.tem@tuta.io>
2023-08-08 11:18:13 +0200

    fix certificates for aws connections (bundle moved in root of the project)

--- backend/src/entities/connection/ssl-certificate/read-certificate.ts
+++ backend/src/entities/connection/ssl-certificate/read-certificate.ts
@@ -1,15 +1,22 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { join } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export async function readSslCertificate(): Promise<string> {
   const fileName = 'global-bundle.pem';
   return new Promise((resolve, reject) => {
-    // eslint-disable-next-line security/detect-non-literal-fs-filename
-    fs.readFile(path.join(process.cwd(), fileName), 'utf8', function (err, data) {
-      if (err) {
-        reject(err);
-      }
-      resolve(data);
-    });
+    fs.readFile(
+      join(__dirname, '..', '..', '..', '..', 'files', 'certificates', fileName),
+      'utf8',
+      function (err, data) {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      },
+    );
   });
 }
