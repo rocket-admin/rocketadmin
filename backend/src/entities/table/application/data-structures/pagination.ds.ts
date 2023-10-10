4cf2283620039b3d25b0f245168df47270172d9c
Artem Niehrieiev <r.tem@tuta.io>
2023-10-10 15:35:37 +0200

    added swagger fot table logs

--- backend/src/entities/table/application/data-structures/pagination.ds.ts
+++ backend/src/entities/table/application/data-structures/pagination.ds.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class PaginationDs {
+  @ApiProperty()
   currentPage: number;
+
+  @ApiProperty()
   lastPage: number;
+
+  @ApiProperty()
   perPage: number;
+
+  @ApiProperty()
   total: number;
 }
