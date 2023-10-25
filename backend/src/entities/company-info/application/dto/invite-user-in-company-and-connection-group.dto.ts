68fb7d0f66d914855ee28286180fb0395b2f873f
Artem Niehrieiev <r.tem@tuta.io>
2023-10-25 14:33:41 +0200

    validations added

--- backend/src/entities/company-info/application/dto/invite-user-in-company-and-connection-group.dto.ts
+++ backend/src/entities/company-info/application/dto/invite-user-in-company-and-connection-group.dto.ts
@@ -1,13 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { UserRoleEnum } from '../../../user/enums/user-role.enum.js';
+import { IsEmail, IsEnum, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class InviteUserInCompanyAndConnectionGroupDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
   @ApiProperty()
   groupId: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   @ApiProperty()
   email: string;
 
+  @IsNotEmpty()
+  @IsEnum(UserRoleEnum)
   @ApiProperty({ enum: UserRoleEnum })
   role: UserRoleEnum;
 }
