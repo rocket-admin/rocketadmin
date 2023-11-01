b568eed73cbdcf74390f1de9492b86051f54d150
Artem Niehrieiev <r.tem@tuta.io>
2023-11-01 12:53:36 +0100

    added validations

--- backend/src/entities/user-actions/dto/create-user-action.dto.ts
+++ backend/src/entities/user-actions/dto/create-user-action.dto.ts
@@ -1,7 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { UserActionEnum } from '../../../enums/index.js';
+import { IsEnum, IsNotEmpty } from 'class-validator';
 
 export class CreateUserActionDto {
   @ApiProperty({ enum: UserActionEnum })
+  @IsNotEmpty()
+  @IsEnum(UserActionEnum)
   message: UserActionEnum;
 }
