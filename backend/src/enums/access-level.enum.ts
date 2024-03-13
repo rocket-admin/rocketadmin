05ebeab91a2835fe80c42626ced8fc8aba426bb0
Artem Niehrieiev <r.tem@tuta.io>
2024-03-13 11:07:35 +0000

    removed "fullaccess" and and uppercase access levels

--- backend/src/enums/access-level.enum.ts
+++ backend/src/enums/access-level.enum.ts
@@ -1,9 +1,5 @@
 export enum AccessLevelEnum {
-  FullAccess = 'FullAccess',
-  fullaccess = 'fullaccess',
-  Readonly = 'Readonly',
   readonly = 'readonly',
-  Edit = 'Edit',
   edit = 'edit',
   visibility = 'visibility',
   add = 'add',
