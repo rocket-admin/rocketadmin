dab2bec18fdb05dcfee325c33f4ff991bb222a5b
Artem Niehrieiev <r.tem@tuta.io>
2024-03-22 09:34:22 +0000

    Refactor filter criteria for empty values in data access objects

--- shared-code/src/data-access-layer/data-access-objects/data-access-object-mysql.ts
+++ shared-code/src/data-access-layer/data-access-objects/data-access-object-mysql.ts
@@ -568,21 +568,16 @@ export class DataAccessObjectMysql extends BasicDataAccessObject implements IDat
               [FilterCriteriaEnum.gte]: '>=',
               [FilterCriteriaEnum.contains]: 'like',
               [FilterCriteriaEnum.icontains]: 'not like',
-              [FilterCriteriaEnum.empty]: ['is', '='],
+              [FilterCriteriaEnum.empty]: 'is',
             };
             const values = {
               [FilterCriteriaEnum.startswith]: `${value}%`,
               [FilterCriteriaEnum.endswith]: `%${value}`,
               [FilterCriteriaEnum.contains]: `%${value}%`,
               [FilterCriteriaEnum.icontains]: `%${value}%`,
-              [FilterCriteriaEnum.empty]: [null, ''],
+              [FilterCriteriaEnum.empty]: null,
             };
-            if (criteria === FilterCriteriaEnum.empty) {
-              builder.where(field, operators[criteria][0], values[criteria][0]);
-              builder.orWhere(field, operators[criteria][1], values[criteria][1]);
-            } else {
-              builder.where(field, operators[criteria], values[criteria] || value);
-            }
+            builder.where(field, operators[criteria], values[criteria] || value);
           }
         }
       })
