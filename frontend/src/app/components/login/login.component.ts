1fc5ac18bc276eb8021b777bb26f68447d44e5db
Lyubov Voloshko <lyubov@voloshko.com>
2024-04-04 12:41:35 +0300

    login: fix 2fa for google login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -37,7 +37,8 @@ export class LoginComponent implements OnInit {
       client_id: "681163285738-e4l0lrv5vv7m616ucrfhnhso9r396lum.apps.googleusercontent.com",
       callback: (authUser) => {
         this.ngZone.run(() => {
-          this._auth.loginWithGoogle(authUser.credential).subscribe(() => {
+          this._auth.loginWithGoogle(authUser.credential).subscribe((res) => {
+            if (res.isTemporary) this.is2FAShown = true;
             this.angulartics2.eventTrack.next({
               action: 'Login: google login success'
             });
