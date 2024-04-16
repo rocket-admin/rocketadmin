e13148f61c80ecfa7b4466b23a93eab1cd521e84
Lyubov Voloshko <lyubov@voloshko.com>
2024-04-16 15:46:11 +0300

    add login with github

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -75,39 +75,11 @@ export class AuthService {
     );
   }
 
-  signUpWithGithubRequest() {
+  signUpWithGithub() {
     const config = this._configuration.getConfig();
 
     location.assign(config.saasURL + '/saas/user/github/registration/request');
   }
-
-  // signUpWithGithub(code: string) {
-  //   const config = this._configuration.getConfig();
-
-  //   return this._http.get<any>(config.saasURL + '/saas/user/github/registration', {
-  //     params: {
-  //       code
-  //     }
-  //   })
-  //   .pipe(
-  //     map(res => {
-  //       this.auth.next(res);
-  //       return res;
-  //     }),
-  //     catchError((err) => {
-  //       console.log(err);
-  //       Sentry.captureException(err);
-  //       this._notifications.showAlert(AlertType.Error, {abstract: err.error.message, details: err.error.originalMessage}, [
-  //         {
-  //           type: AlertActionType.Button,
-  //           caption: 'Dismiss',
-  //           action: (id: number) => this._notifications.dismissAlert()
-  //         }
-  //       ]);
-  //       return EMPTY;
-  //     })
-  //   );
-  // }
 
   loginUser(userData: ExistingAuthUser) {
     return this._http.post<any>('/user/login', userData)
@@ -173,6 +145,12 @@ export class AuthService {
         return EMPTY;
       })
     );
+  }
+
+  loginWithGithub() {
+    const config = this._configuration.getConfig();
+
+    location.assign(config.saasURL + '/saas/user/github/login/request');
   }
 
   requestEmailVerifications() {
