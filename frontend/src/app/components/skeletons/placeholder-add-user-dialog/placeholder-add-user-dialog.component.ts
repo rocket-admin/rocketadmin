edab849e60fc449c96f6bb632fcd083e14f6ed9e
Lyubov Voloshko <lyubov@voloshko.com>
2024-05-16 14:44:02 +0300

    add link to Company into popup text

---
+++ frontend/src/app/components/skeletons/placeholder-add-user-dialog/placeholder-add-user-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placeholder-add-user-dialog',
+  templateUrl: './placeholder-add-user-dialog.component.html',
+  styleUrls: ['./placeholder-add-user-dialog.component.css']
+})
+export class PlaceholderAddUserDialogComponent {
+
+}
