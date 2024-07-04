3375583ed6db04007fd04aca5b150f91d20d9f82
Artem Niehrieiev <r.tem@tuta.io>
2024-07-04 09:42:22 +0000

    chore: Update "created_at" column in "table_actions" table to have a default timestamp

--- backend/src/migrations/1719404444515-AddDefaultTimestampInTableActionEntity.ts
+++ backend/src/migrations/1719404444515-AddDefaultTimestampInTableActionEntity.ts
@@ -4,6 +4,7 @@ export class AddDefaultTimestampInTableActionEntity1719404444515 implements Migr
   name = 'AddDefaultTimestampInTableActionEntity1719404444515';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`UPDATE "table_actions" SET "created_at" = now() WHERE "created_at" IS NULL`);
     await queryRunner.query(`ALTER TABLE "table_actions" ALTER COLUMN "created_at" SET NOT NULL`);
     await queryRunner.query(`ALTER TABLE "table_actions" ALTER COLUMN "created_at" SET DEFAULT now()`);
   }
