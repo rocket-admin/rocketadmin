9a04d39a5c32034cdba6919cbf99fe99e15d4b14
Artem Niehrieiev <r.tem@tuta.io>
2024-07-10 14:12:18 +0000

    refactor: Update connection routes to use connectionId instead of slug

--- backend/src/decorators/slug-verification.decorator.ts
+++ backend/src/decorators/slug-verification.decorator.ts
@@ -4,12 +4,16 @@ import { Messages } from '../exceptions/text/messages.js';
 import { buildBadRequestException } from '../guards/utils/index.js';
 import { ValidationHelper } from '../helpers/validators/validation-helper.js';
 
-export const VerificationString = createParamDecorator((data: any, ctx: ExecutionContext): string => {
-  const request: IRequestWithCognitoInfo = ctx.switchToHttp().getRequest();
-  const verificationString: string = request.params?.slug;
-  const isValidString = ValidationHelper.isValidVerificationString(verificationString);
-  if (isValidString) {
-    return verificationString;
-  }
-  throw buildBadRequestException(Messages.VERIFICATION_STRING_INCORRECT);
-});
+type SlugVerificationType = 'verificationString';
+export const VerificationString = createParamDecorator(
+  (paramName: SlugVerificationType, ctx: ExecutionContext): string => {
+    const request: IRequestWithCognitoInfo = ctx.switchToHttp().getRequest();
+    // eslint-disable-next-line security/detect-object-injection
+    const verificationString: string = request.params?.[paramName] || request.params?.slug;
+    const isValidString = ValidationHelper.isValidVerificationString(verificationString);
+    if (isValidString) {
+      return verificationString;
+    }
+    throw buildBadRequestException(Messages.VERIFICATION_STRING_INCORRECT);
+  },
+);
