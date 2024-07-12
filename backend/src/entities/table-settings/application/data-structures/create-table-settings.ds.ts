95dd402cd6c9f84ef3c01883d647c4c3ccf54c52
Artem Niehrieiev <r.tem@tuta.io>
2024-07-12 13:53:30 +0000

    refactor: Update CreateTableSettingsDs API properties

--- backend/src/entities/table-settings/application/data-structures/create-table-settings.ds.ts
+++ backend/src/entities/table-settings/application/data-structures/create-table-settings.ds.ts
@@ -5,10 +5,10 @@ import { TableActionEntity } from '../../../table-actions/table-actions-module/t
 import { TableWidgetEntity } from '../../../widget/table-widget.entity.js';
 
 export class CreateTableSettingsDs {
-  @ApiProperty({ isArray: true })
+  @ApiProperty({ isArray: true, type: 'string' })
   autocomplete_columns: Array<string>;
 
-  @ApiProperty({ isArray: true })
+  @ApiProperty({ isArray: true, type: 'string', required: false })
   columns_view?: Array<string>;
 
   connection_id: string;
@@ -19,16 +19,16 @@ export class CreateTableSettingsDs {
   @ApiProperty()
   display_name: string;
 
-  @ApiProperty({ isArray: true })
+  @ApiProperty({ isArray: true, type: 'string' })
   excluded_fields: Array<string>;
 
-  @ApiProperty({ isArray: true })
+  @ApiProperty({ isArray: true, type: 'string' })
   identification_fields: Array<string>;
 
   @ApiProperty()
   identity_column: string;
 
-  @ApiProperty({ isArray: true })
+  @ApiProperty({ isArray: true, type: 'string' })
   list_fields: Array<string>;
 
   @ApiProperty()
@@ -42,16 +42,16 @@ export class CreateTableSettingsDs {
   @ApiProperty()
   ordering_field: string;
 
-  @ApiProperty({ isArray: true })
+  @ApiProperty({ isArray: true, type: 'string' })
   readonly_fields: Array<string>;
 
-  @ApiProperty({ isArray: true })
+  @ApiProperty({ isArray: true, type: 'string' })
   search_fields: Array<string>;
 
-  @ApiProperty({ isArray: true })
+  @ApiProperty({ isArray: true, type: 'string' })
   sortable_by: Array<string>;
 
-  @ApiProperty({ isArray: true })
+  @ApiProperty({ isArray: true, type: 'string' })
   sensitive_fields: Array<string>;
 
   table_name: string;
