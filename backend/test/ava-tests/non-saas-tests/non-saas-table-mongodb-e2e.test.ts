f2bea4fdb0869916c497363e22ae91b275147257
Artem Niehrieiev <r.tem@tuta.io>
2024-07-23 11:26:37 +0000

    chore: Set SAAS environment variable in non-SAAS table MongoDB e2e test

--- backend/test/ava-tests/non-saas-tests/non-saas-table-mongodb-e2e.test.ts
+++ backend/test/ava-tests/non-saas-tests/non-saas-table-mongodb-e2e.test.ts
@@ -25,6 +25,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { join } from 'path';
 import { send } from 'process';
+import { setSaasEnvVariable } from '../../utils/set-saas-env-variable.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -36,6 +37,7 @@ const testTables: Array<string> = [];
 let currentTest;
 
 test.before(async () => {
+  setSaasEnvVariable();
   const moduleFixture = await Test.createTestingModule({
     imports: [ApplicationModule, DatabaseModule],
     providers: [DatabaseService, TestUtils],
