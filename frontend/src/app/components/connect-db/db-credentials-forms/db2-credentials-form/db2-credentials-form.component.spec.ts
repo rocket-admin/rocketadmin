f62d8ecb8ba0c55465d1df30eb74abb791e10ecb
Lyubov Voloshko <lyubov@voloshko.com>
2024-12-16 18:43:45 +0200

    db2 creds: fix initial test

--- frontend/src/app/components/connect-db/db-credentials-forms/db2-credentials-form/db2-credentials-form.component.spec.ts
+++ frontend/src/app/components/connect-db/db-credentials-forms/db2-credentials-form/db2-credentials-form.component.spec.ts
@@ -1,19 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Db2CredentialsFormComponent } from './db2-credentials-form.component';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
-describe('Db2CredentialsFormComponent', () => {
+fdescribe('Db2CredentialsFormComponent', () => {
   let component: Db2CredentialsFormComponent;
   let fixture: ComponentFixture<Db2CredentialsFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Db2CredentialsFormComponent]
+      declarations: [Db2CredentialsFormComponent],
+      imports: [
+        FormsModule,
+        MatCheckboxModule
+      ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(Db2CredentialsFormComponent);
     component = fixture.componentInstance;
+
+    component.connection = {
+      id: "12345678"
+    } as any;
+
     fixture.detectChanges();
   });
 
