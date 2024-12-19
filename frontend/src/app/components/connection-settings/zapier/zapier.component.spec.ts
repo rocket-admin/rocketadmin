08945ea2988289006f78f9fc93692edfa9e94999
Lyubov Voloshko <lyubov@voloshko.com>
2024-12-19 17:34:53 +0200

    add zapier widget to connection settings

---
+++ frontend/src/app/components/connection-settings/zapier/zapier.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ZapierComponent } from './zapier.component';
+
+describe('ZapierComponent', () => {
+  let component: ZapierComponent;
+  let fixture: ComponentFixture<ZapierComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ZapierComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ZapierComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
