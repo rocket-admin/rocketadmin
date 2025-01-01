ebc2e5e3cb5ae31cb1218009744170147df97baa
Lyubov Voloshko <lyubov@voloshko.com>
2025-01-01 18:13:20 +0200

    apply dark theme

--- frontend/src/app/components/ui-components/ip-address-button/ip-address-button.component.ts
+++ frontend/src/app/components/ui-components/ip-address-button/ip-address-button.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { CdkCopyToClipboard } from '@angular/cdk/clipboard';
 import { NotificationsService } from 'src/app/services/notifications.service';
 
 @Component({
   selector: 'app-ip-address-button',
   templateUrl: './ip-address-button.component.html',
-  styleUrls: ['./ip-address-button.component.css']
+  styleUrls: ['./ip-address-button.component.css'],
+  imports: [CdkCopyToClipboard]
 })
 export class IpAddressButtonComponent implements OnInit {
   @Input() ip: string;
