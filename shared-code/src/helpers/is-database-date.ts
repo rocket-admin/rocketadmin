4db3c75df41c14c9c73aa4096265626ca8c5ccd0
Artem Niehrieiev <r.tem@tuta.io>
2025-02-26 11:11:31 +0000

    Enhance OracleDB data access with date/time type detection and proper formatting in queries for better filtering capabilities

--- shared-code/src/helpers/is-database-date.ts
+++ shared-code/src/helpers/is-database-date.ts
@@ -18,8 +18,29 @@ export function isOracleDateOrTimeType(type: string): boolean {
   if (type.toLowerCase().includes('timestamp')) {
     return true;
   }
-  const dateTypes = ['date', 'timestamp', 'timestamp with time zone', 'timestamp with local time zone'];
+  const dateTypes = [
+    'date',
+    'timestamp',
+    'timestamp with time zone',
+    'timestamp with local time zone',
+    'timestamp(6) with local time zone',
+    'timestamp(0) with local time zone',
+  ];
   return dateTypes.includes(type.toLowerCase());
+}
+
+export function isOracleTimeType(type: string): boolean {
+  return [
+    'timestamp',
+    'timestamp with time zone',
+    'timestamp with local time zone',
+    'timestamp(6) with local time zone',
+    'timestamp(0) with local time zone',
+  ].includes(type.toLowerCase());
+}
+
+export function isOracleDateType(type: string): boolean {
+  return ['date'].includes(type.toLowerCase());
 }
 
 export function isOracleDateStringByRegexp(value: string): boolean {
