52d265b57ebb4843c87453bcc24ca38687716a51
Artem Niehrieiev <r.tem@tuta.io>
2025-04-11 08:17:29 +0000

    Remove unused RegisteredCompanyUserInviteGroupDS class and associated methods for user invitation in SaaS company service

--- backend/src/microservices/gateways/saas-gateway.ts/saas-company-gateway.service.ts
+++ backend/src/microservices/gateways/saas-gateway.ts/saas-company-gateway.service.ts
@@ -5,50 +5,11 @@ import { isObjectEmpty } from '../../../helpers/is-object-empty.js';
 import { SuccessResponse } from '../../saas-microservice/data-structures/common-responce.ds.js';
 import { BaseSaasGatewayService } from './base-saas-gateway.service.js';
 import { FoundSassCompanyInfoDS } from './data-structures/found-saas-company-info.ds.js';
-import { RegisteredCompanyUserInviteGroupDS } from './data-structures/registered-company-when-user-invite-group.ds.js';
 
 @Injectable()
 export class SaasCompanyGatewayService extends BaseSaasGatewayService {
   constructor() {
     super();
-  }
-
-  public async registerCompanyWhenUserInviteInGroup(
-    userId: string,
-    userEmail: string,
-    gitHubId: number,
-  ): Promise<RegisteredCompanyUserInviteGroupDS | null> {
-    const registrationResult = await this.sendRequestToSaaS(`/webhook/company/register`, 'POST', {
-      userId,
-      userEmail: userEmail?.toLowerCase(),
-      gitHubId,
-    });
-
-    if (registrationResult.status > 299) {
-      throw new HttpException(
-        {
-          message: Messages.FAILED_REGISTER_COMPANY_AND_INVITE_USER_IN_GROUP_UNHANDLED_ERROR,
-          originalMessage: registrationResult?.body?.message ? registrationResult.body.message : undefined,
-        },
-        registrationResult.status || 500,
-      );
-    }
-
-    if (isObjectEmpty(registrationResult.body)) {
-      return null;
-    }
-    return {
-      companyId: registrationResult.body.id as string,
-      userId: registrationResult.body.users[0].id as string,
-    };
-  }
-
-  public async registerEmptyCompany(
-    userId: string,
-    userEmail: string,
-    gitHubId: number,
-  ): Promise<RegisteredCompanyUserInviteGroupDS | null> {
-    return await this.registerCompanyWhenUserInviteInGroup(userId, userEmail?.toLowerCase(), gitHubId);
   }
 
   public async getCompanyInfo(companyId: string): Promise<FoundSassCompanyInfoDS | null> {
@@ -116,12 +77,6 @@ export class SaasCompanyGatewayService extends BaseSaasGatewayService {
   }
 
   private isDataFoundSassCompanyInfoDS(data: unknown): data is FoundSassCompanyInfoDS {
-    return (
-      typeof data === 'object' &&
-      data !== null &&
-      'id' in data &&
-      'createdAt' in data &&
-      'updatedAt' in data
-    );
+    return typeof data === 'object' && data !== null && 'id' in data && 'createdAt' in data && 'updatedAt' in data;
   }
 }
