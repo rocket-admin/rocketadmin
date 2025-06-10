c224915bb860c3cdb9557275862b58439fda688a
Lyubov Voloshko <lyubov@voloshko.com>
2025-06-10 21:25:42 +0300

    fix unit tests

--- frontend/src/app/components/dashboard/db-table/db-table.component.spec.ts
+++ frontend/src/app/components/dashboard/db-table/db-table.component.spec.ts
@@ -90,7 +90,7 @@ describe('DbTableComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DbTableComponent);
     component = fixture.componentInstance;
-    component.table = new TablesDataSource({} as any, {} as any, {} as any);
+    component.table = new TablesDataSource({} as any, {} as any, {} as any, {} as any);
     component.selection = new SelectionModel<any>(true, []);
     component.filterComparators = mockFilterComparators;
     fixture.autoDetectChanges();
