f23801762c4f07ec816958c0e8fefd4dcfdc5521
Andrii Kostenko <andrey@kostenko.name>
2025-06-19 10:48:45 +0000

    removed password value (backend)

--- backend/src/entities/table/utils/hash-passwords-in-row.util.ts
+++ backend/src/entities/table/utils/hash-passwords-in-row.util.ts
@@ -2,6 +2,7 @@ import { TableWidgetEntity } from '../../widget/table-widget.entity.js';
 import { WidgetTypeEnum } from '../../../enums/index.js';
 import { IPasswordWidgetParams } from '../../widget/table-widget.interface.js';
 import { Encryptor } from '../../../helpers/encryption/encryptor.js';
+import { Constants } from '../../../helpers/constants/constants.js';
 import JSON5 from 'json5';
 
 export async function hashPasswordsInRowUtil(
@@ -14,6 +15,12 @@ export async function hashPasswordsInRowUtil(
     try {
       const widgetParams = JSON5.parse(widget.widget_params) as unknown as IPasswordWidgetParams;
       const fieldValue = row[widget.field_name];
+
+      // Skip processing if the field value is the removed password placeholder
+      if (fieldValue === Constants.REMOVED_PASSWORD_VALUE) {
+        delete row[widget.field_name];
+        continue;
+      }
 
       if (fieldValue !== undefined && fieldValue !== null && fieldValue !== '' && widgetParams.encrypt) {
         row[widget.field_name] = await Encryptor.processDataWithAlgorithm(fieldValue as any, widgetParams.algorithm);
