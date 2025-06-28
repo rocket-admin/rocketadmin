d62dcf266c1bae5ecfd0042039ca3fe00046d92a
Andrii Kostenko <andrey@kostenko.name>
2025-06-28 20:39:18 +0000

    material phone number input

--- frontend/src/app/components/ui-components/row-fields/phone/phone.component.ts
+++ frontend/src/app/components/ui-components/row-fields/phone/phone.component.ts
@@ -1,6 +1,5 @@
 import { Component, Injectable, Input, OnInit, AfterViewInit } from '@angular/core';
-import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
-import { CountryISO, SearchCountryField, PhoneNumberFormat } from 'ngx-intl-tel-input';
+import { NgxMatInputTelComponent } from 'ngx-mat-input-tel';
 
 import { BaseRowFieldComponent } from '../base-row-field/base-row-field.component';
 import { FormsModule } from '@angular/forms';
@@ -12,21 +11,17 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   selector: 'app-row-phone',
   templateUrl: './phone.component.html',
   styleUrls: ['./phone.component.css'],
-  imports: [MatFormFieldModule, FormsModule, NgxIntlTelInputModule]
+  imports: [MatFormFieldModule, FormsModule, NgxMatInputTelComponent]
 })
 export class PhoneRowComponent extends BaseRowFieldComponent implements OnInit, AfterViewInit {
   @Input() value: string;
 
   static type = 'phone';
 
-  SearchCountryField = SearchCountryField;
-  CountryISO = CountryISO;
-  PhoneNumberFormat = PhoneNumberFormat;
-  preferredCountries: CountryISO[] = [CountryISO.UnitedStates, CountryISO.UnitedKingdom];
+  preferredCountries: string[] = ['US', 'GB'];
   enablePlaceholder: boolean = true;
   enableAutoCountrySelect: boolean = true;
   phoneValidation: boolean = true;
-  numberFormat: PhoneNumberFormat = PhoneNumberFormat.International;
 
   ngOnInit(): void {
     super.ngOnInit();
@@ -42,7 +37,7 @@ export class PhoneRowComponent extends BaseRowFieldComponent implements OnInit, 
       const params = this.widgetStructure.widget_params;
       
       if (params.preferred_countries && Array.isArray(params.preferred_countries)) {
-        this.preferredCountries = params.preferred_countries.map(country => CountryISO[country as keyof typeof CountryISO] || CountryISO.UnitedStates);
+        this.preferredCountries = params.preferred_countries;
       }
       
       if (typeof params.enable_placeholder === 'boolean') {
@@ -56,20 +51,11 @@ export class PhoneRowComponent extends BaseRowFieldComponent implements OnInit, 
       if (typeof params.phone_validation === 'boolean') {
         this.phoneValidation = params.phone_validation;
       }
-      
-      if (params.format === 'national') {
-        this.numberFormat = PhoneNumberFormat.National;
-      }
     }
   }
 
-  onPhoneNumberChange(phoneNumber: any): void {
-    if (phoneNumber && phoneNumber.internationalNumber) {
-      this.value = phoneNumber.internationalNumber;
-      this.onFieldChange.emit(this.value);
-    } else {
-      this.value = '';
-      this.onFieldChange.emit(this.value);
-    }
+  onPhoneNumberChange(phoneNumber: string): void {
+    this.value = phoneNumber;
+    this.onFieldChange.emit(this.value);
   }
 }
