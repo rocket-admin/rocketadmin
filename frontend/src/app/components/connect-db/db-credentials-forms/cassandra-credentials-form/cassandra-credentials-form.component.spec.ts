5107c85943a6869070c32f63c8ab8958529082f0
Lyubov Voloshko <lyubov@voloshko.com>
2025-07-29 18:07:01 +0300

    cassandra: fix unit test

--- frontend/src/app/components/connect-db/db-credentials-forms/cassandra-credentials-form/cassandra-credentials-form.component.spec.ts
+++ frontend/src/app/components/connect-db/db-credentials-forms/cassandra-credentials-form/cassandra-credentials-form.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Angulartics2Module } from 'angulartics2';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CassandraCredentialsFormComponent } from './cassandra-credentials-form.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('CassandraCredentialsFormComponent', () => {
   let component: CassandraCredentialsFormComponent;
@@ -8,12 +11,22 @@ describe('CassandraCredentialsFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CassandraCredentialsFormComponent]
+      imports: [
+        BrowserAnimationsModule,
+        Angulartics2Module.forRoot(),
+        CassandraCredentialsFormComponent
+      ],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(CassandraCredentialsFormComponent);
     component = fixture.componentInstance;
+
+    component.connection = {
+      id: "12345678"
+    } as any;
+
     fixture.detectChanges();
   });
 
