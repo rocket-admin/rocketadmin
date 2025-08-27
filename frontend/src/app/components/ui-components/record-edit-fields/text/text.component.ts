791288b8efdc1aa631af6bbac01f7a0642f80971
Andrii Kostenko <andrey@kostenko.name>
2025-08-27 08:53:43 +0000

    form validation WIP

--- frontend/src/app/components/ui-components/record-edit-fields/text/text.component.ts
+++ frontend/src/app/components/ui-components/record-edit-fields/text/text.component.ts
@@ -4,6 +4,12 @@ import { BaseEditFieldComponent } from '../base-row-field/base-row-field.compone
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+<<<<<<< Updated upstream
+=======
+import { CommonModule } from '@angular/common';
+import { TextValidatorDirective } from 'src/app/directives/text-validator.directive';
+import { FieldValidationDirective } from 'src/app/directives/field-validation.directive';
+>>>>>>> Stashed changes
 
 @Injectable()
 
@@ -11,7 +17,11 @@ import { MatInputModule } from '@angular/material/input';
   selector: 'app-edit-text',
   templateUrl: './text.component.html',
   styleUrls: ['./text.component.css'],
+<<<<<<< Updated upstream
   imports: [MatFormFieldModule, MatInputModule, FormsModule]
+=======
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, FormsModule, TextValidatorDirective, FieldValidationDirective]
+>>>>>>> Stashed changes
 })
 export class TextEditComponent extends BaseEditFieldComponent {
   @Input() value: string;
