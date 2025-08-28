de73678bc942ba6908330c9c6ec5162971085fb0
Lyubov Voloshko <lyubov@voloshko.com>
2025-08-28 12:27:33 +0300

    text validator: take out text validator from directive

--- frontend/src/app/validators/url.validator.ts
+++ frontend/src/app/validators/url.validator.ts
@@ -1,13 +1,11 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
-import isFQDN from 'validator/lib/isFQDN';
-
 export function urlValidation(prefix: string = ''):ValidatorFn {
     return (control: AbstractControl) : ValidationErrors | null=> {
 
         if (control.value) {
             let url = (control.value as string);
-            
+
             // If there's a prefix, prepend it to the URL for validation
             const fullUrl = prefix ? prefix + url : url;
 
