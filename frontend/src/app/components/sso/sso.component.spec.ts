fe415bcc9088f36ce500f302b8f5976d0372e870
Lyubov Voloshko <lyubov@voloshko.com>
2025-09-08 17:14:24 +0300

    WIP fix unit tests

--- frontend/src/app/components/sso/sso.component.spec.ts
+++ frontend/src/app/components/sso/sso.component.spec.ts
@@ -1,14 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SsoComponent } from './sso.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute, convertToParamMap, provideRouter, Router } from '@angular/router';
+import { CompanyService } from 'src/app/services/company.service';
+import { of } from 'rxjs';
 
-describe('SsoComponent', () => {
+fdescribe('SsoComponent', () => {
   let component: SsoComponent;
   let fixture: ComponentFixture<SsoComponent>;
+  let companyServiceSpy: jasmine.SpyObj<CompanyService>;
+
+  const mockActivatedRoute = {
+    snapshot: {
+      paramMap: convertToParamMap({
+        'company-id': '123'
+      })
+    }
+  };
+
+  // Mock router state
+  const mockRouter = {
+    routerState: {
+      snapshot: {
+        root: {
+          firstChild: {
+            params: {
+              'company-id': '123'
+            }
+          }
+        }
+      }
+    },
+    navigate: jasmine.createSpy('navigate')
+  };
 
   beforeEach(async () => {
+    companyServiceSpy = jasmine.createSpyObj('CompanyService', ['fetchSamlConfiguration', 'createSamlConfiguration', 'updateSamlConfiguration']);
+    companyServiceSpy.fetchSamlConfiguration.and.returnValue(of([]));
+
     await TestBed.configureTestingModule({
-      imports: [SsoComponent]
+      imports: [
+        SsoComponent,
+        HttpClientTestingModule
+      ],
+      providers: [
+        provideRouter([]),
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: Router, useValue: mockRouter },
+        { provide: CompanyService, useValue: companyServiceSpy }
+      ]
     })
     .compileComponents();
 
