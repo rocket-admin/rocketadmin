8109d6c2bff89750a5f8af6c9923946f0865cf17
Andrii Kostenko <andrey@kostenko.name>
2025-11-28 13:04:40 +0000

    fix test runner

--- backend/src/entities/user-secret/repository/user-secret-repository.extension.ts
+++ backend/src/entities/user-secret/repository/user-secret-repository.extension.ts
@@ -1,11 +1,10 @@
-import { Like } from 'typeorm';
+import { FindOptionsWhere, Like } from 'typeorm';
 import { UserSecretEntity } from '../user-secret.entity.js';
 import { IUserSecretRepository } from './user-secret-repository.interface.js';
 
 export const userSecretRepositoryExtension: IUserSecretRepository = {
   async findSecretBySlugAndCompanyId(slug: string, companyId: string): Promise<UserSecretEntity | null> {
-    const self = this as any;
-    return self.findOne({
+    return this.findOne({
       where: {
         slug,
         companyId,
@@ -17,8 +16,7 @@ export const userSecretRepositoryExtension: IUserSecretRepository = {
     companyId: string,
     options: { page: number; limit: number; search?: string },
   ): Promise<[UserSecretEntity[], number]> {
-    const self = this as any;
-    const where: any = {
+    const where: FindOptionsWhere<UserSecretEntity> = {
       companyId,
     };
 
@@ -26,7 +24,7 @@ export const userSecretRepositoryExtension: IUserSecretRepository = {
       where.slug = Like(`%${options.search}%`);
     }
 
-    return self.findAndCount({
+    return this.findAndCount({
       where,
       order: {
         createdAt: 'DESC',
--- backend/src/entities/user-secret/repository/user-secret-repository.interface.ts
+++ backend/src/entities/user-secret/repository/user-secret-repository.interface.ts
@@ -1,8 +1,14 @@
+import { Repository } from 'typeorm';
 import { UserSecretEntity } from '../user-secret.entity.js';
 
 export interface IUserSecretRepository {
-  findSecretBySlugAndCompanyId(slug: string, companyId: string): Promise<UserSecretEntity | null>;
+  findSecretBySlugAndCompanyId(
+    this: Repository<UserSecretEntity>,
+    slug: string,
+    companyId: string,
+  ): Promise<UserSecretEntity | null>;
   findSecretsForCompany(
+    this: Repository<UserSecretEntity>,
     companyId: string,
     options: { page: number; limit: number; search?: string },
   ): Promise<[UserSecretEntity[], number]>;
