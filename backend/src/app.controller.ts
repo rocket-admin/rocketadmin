777f5413a06813031bde95075d6c620c403f1c32
Artem Niehrieiev <r.tem@tuta.io>
2025-12-22 14:55:55 +0000

    refactor: format code style in app.controller.ts and update biome.json settings

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -9,13 +9,13 @@ import { IGetHello } from './use-cases-app/use-cases-app.interface.js';
 @Controller()
 @ApiTags('app')
 export class AppController {
-  constructor(
-    @Inject(UseCaseType.GET_HELLO)
-    private readonly getHelloUseCase: IGetHello,
-  ) {}
+	constructor(
+		@Inject(UseCaseType.GET_HELLO)
+		private readonly getHelloUseCase: IGetHello,
+	) {}
 
-  @Get('/hello')
-  async getHello(): Promise<string> {
-    return this.getHelloUseCase.execute(undefined, InTransactionEnum.OFF);
-  }
+	@Get('/hello')
+	async getHello(): Promise<string> {
+		return this.getHelloUseCase.execute(undefined, InTransactionEnum.OFF);
+	}
 }
