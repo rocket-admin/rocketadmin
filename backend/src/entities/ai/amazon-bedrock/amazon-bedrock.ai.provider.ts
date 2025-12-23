ae8300ca87de25c5bd1d1d2ddd8bdcf455a8ed50
Artem Niehrieiev <r.tem@tuta.io>
2025-12-23 13:12:55 +0000

    refactor: enhance logging and code formatting in SharedJobsService

--- backend/src/entities/ai/amazon-bedrock/amazon-bedrock.ai.provider.ts
+++ backend/src/entities/ai/amazon-bedrock/amazon-bedrock.ai.provider.ts
@@ -4,38 +4,40 @@ import { IAIProvider } from './ai-provider.interface.js';
 
 @Injectable()
 export class AmazonBedrockAiProvider implements IAIProvider {
-  private readonly bedrockRuntimeClient: BedrockRuntimeClient;
-  private readonly modelId: string = 'global.anthropic.claude-sonnet-4-5-20250929-v1:0';
-  private readonly temperature: number = 0.7;
-  private readonly maxTokens: number = 1024;
-  private readonly region: string = 'us-west-2';
-  private readonly topP: number = 0.9;
+	private readonly bedrockRuntimeClient: BedrockRuntimeClient;
+	private readonly modelId: string = 'global.anthropic.claude-sonnet-4-5-20250929-v1:0';
+	private readonly temperature: number = 0.7;
+	private readonly maxTokens: number = 1024;
+	private readonly region: string = 'us-west-2';
+	private readonly topP: number = 0.9;
 
-  constructor() {
-    this.bedrockRuntimeClient = new BedrockRuntimeClient({
-      region: this.region,
-    });
-  }
-  public async generateResponse(prompt: string): Promise<string> {
-    const conversation = [
-      {
-        role: 'user' as const,
-        content: [{ text: prompt }],
-      },
-    ];
+	constructor() {
+		this.bedrockRuntimeClient = new BedrockRuntimeClient({
+			region: this.region,
+		});
+	}
+	public async generateResponse(prompt: string): Promise<string> {
+		const conversation = [
+			{
+				role: 'user' as const,
+				content: [{ text: prompt }],
+			},
+		];
 
-    const command = new ConverseCommand({
-      modelId: this.modelId,
-      messages: conversation,
-      inferenceConfig: { maxTokens: this.maxTokens, temperature: this.temperature, topP: this.topP },
-    });
-    try {
-      const response = await this.bedrockRuntimeClient.send(command);
-      const responseText = response.output.message?.content[0].text;
-      return responseText || 'No response generated.';
-    } catch (error) {
-      console.error('Error generating AI response:', error);
-      throw new Error('Failed to generate AI response.');
-    }
-  }
+		const command = new ConverseCommand({
+			modelId: this.modelId,
+			messages: conversation,
+			inferenceConfig: { maxTokens: this.maxTokens, temperature: this.temperature, topP: this.topP },
+		});
+		try {
+			const response = await this.bedrockRuntimeClient.send(command);
+			console.info('AI response received from Amazon Bedrock.');
+			const responseText = response.output.message?.content[0].text;
+			console.info('AI response text. ', responseText);
+			return responseText || 'No response generated.';
+		} catch (error) {
+			console.error('Error generating AI response:', error);
+			throw new Error('Failed to generate AI response.');
+		}
+	}
 }
