c509647dba8f1ded682916db7be416f78eb9caeb
Lyubov Voloshko <lyubov@voloshko.com>
2026-01-06 13:20:46 +0000

    fix access to browser from container

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: false,
 	saas: false,
 	apiRoot: 'https://rocketadmin-dev.tail9f8b2.ts.net/api',
-	saasURL: 'https://app.rocketadmin.com',
+	saasURL: 'https://rocketadmin-dev.tail9f8b2.ts.net',
 	saasHostnames: ['localhost', '127.0.0.1'],
 	stagingHost: '',
 	version: '0.0.0',
