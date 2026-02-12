ea9a9cf02a50ef85f80f857d6e4f4715d20b4456
Lyubov Voloshko <lyubov@voloshko.com>
2026-02-12 14:23:26 +0000

    company settings: set up visibility of elements depending on company role and saas/self-hosted version

--- frontend/src/app/components/upgrade/upgrade.component.ts
+++ frontend/src/app/components/upgrade/upgrade.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { AlertComponent } from '../ui-components/alert/alert.component';
 import { CommonModule } from '@angular/common';
+import { CompanyMemberRole } from 'src/app/models/company';
 import { CompanyService } from 'src/app/services/company.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
@@ -11,6 +12,7 @@ import { PaymentService } from 'src/app/services/payment.service';
 import { PlanKey } from 'src/app/models/plans';
 import { ProfileSidebarComponent } from '../profile/profile-sidebar/profile-sidebar.component';
 import { RouterModule } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 import plans from '../../consts/plans';
 
 @Component({
@@ -39,6 +41,7 @@ export class UpgradeComponent implements OnInit {
   public portalLink: string = null;
   public companyId: string;
   public submitting = false;
+  public isAdmin = false;
 
   databases = [
     {
@@ -175,10 +178,14 @@ export class UpgradeComponent implements OnInit {
   constructor(
     private _paymentService: PaymentService,
     private _company: CompanyService,
+    private _userService: UserService,
   ) { }
 
   ngOnInit(): void {
     this.getCompanyPaln();
+    this._userService.cast.subscribe((user) => {
+      this.isAdmin = user.role === CompanyMemberRole.CAO;
+    });
   }
 
   getCompanyPaln() {
